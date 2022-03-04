195aca055be794c36ca6a264ca0f67abaeccd484
Kambale Kibeho <dessaikibeho@gmail.com>
2022-03-04 17:51:12 +0300

    my

--- Style&&Logic/javascript/javascript.js
+++ Style&&Logic/javascript/javascript.js
@@ -1,10 +1,11 @@
 
 function openNav() {
-  document.getElementById("collapse1").style.width = "250px";
-  document.getElementById("").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.display = "inline";
+  
+  
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.display = "none";
-  document.getElementById("collapse1").style.marginLeft= "0";
+
 }
