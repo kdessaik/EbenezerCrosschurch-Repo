c07c8359ccb3fdfe0a68b6b98c9acc77afcf97a0
Kambale Kibeho <dessaikibeho@gmail.com>
2022-03-07 17:19:14 +0300

    my cha

--- Style&&Logic/javascript/javascript.js
+++ Style&&Logic/javascript/javascript.js
@@ -1,11 +1,13 @@
 
 function openNav() {
   document.getElementById("mySidebar").style.display = "inline";
+  document.getElementById('imgFlue1').style.opacity='0.1';
   
   
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.display = "none";
+  document.getElementById('imgFlue1').style.opacity='0.6';
 
 }
