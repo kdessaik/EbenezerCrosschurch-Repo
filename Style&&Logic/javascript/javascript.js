7f7a30f783d0c95d64cb6bcda153e570da630a8e
Kambale Kibeho <dessaikibeho@gmail.com>
2022-03-09 14:35:41 +0300

    my change

--- Style&&Logic/javascript/javascript.js
+++ Style&&Logic/javascript/javascript.js
@@ -2,7 +2,8 @@
 function openNav() {
   document.getElementById("mySidebar").style.display = "inline";
   document.getElementById('imgFlue1').style.opacity='0.1';
-  document.getElementById('imagelogo2').style.width='0%'
+  document.getElementById('imagelogo2').style.width='0%';
+  document.getElementById
   
 }
 
