a6e3e22cb6346a17c05084f3507f3fbf4f59438f
kdessaik <kdessaikibeho@gmail.com>
2022-04-10 17:08:48 +0100

    dessai

--- Style&&Logic/javascript/javascript.js
+++ Style&&Logic/javascript/javascript.js
@@ -1,14 +1,15 @@
 
 function openNav() {
   document.getElementById("mySidebar").style.display = "inline",
-  document.getElementById('imagelogo2').style.width='0%';
+  document.getElementById('imagelogo2').style.width='10%';
+  document.getElementById('Title111').style.display='none';
   
 }
 
  function closeNav() {
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById('imagelogo2').style.width='30%'
-
+  document.getElementById('Title111').style.display='block';
 }
 
 
